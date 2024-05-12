b697c446158120ac1f6df0880d750a81f0d5bf2c
hlwalZ <ajustytmy@gmail.com>
2024-05-12 18:24:34 +0200

    useRef toegevoegd maar misschien veranderen naar een onSubmit functie meegeven aan form?

--- client/src/pages/AddHours.tsx
+++ client/src/pages/AddHours.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+
+let uren = 0;
 
 const submitForm = (data: any) => {
   console.log(data);
@@ -6,45 +8,57 @@ const submitForm = (data: any) => {
 
 const AddHours = () => {
   // we hadden ook useState kunnen gebruiken voor het updaten van de pagina, alleen render je opnieuw daarmee een volledig component, nu voege
-  useEffect(() => {
-    const rangeInput: any = document.getElementById("uren-geleerd");
-    const outputParagraph: any = document.getElementById("output-geleerd");
-    const rangeInputLeer: any = document.getElementById("uren-gewerkt");
-    const outputParagraphLeer: any = document.getElementById("output-gewerkt");
+  // useEffect(() => {
+  //   const rangeInput: any = document.getElementById("uren-geleerd");
+  //   const outputParagraph: any = document.getElementById("output-geleerd");
+  //   const rangeInputLeer: any = document.getElementById("uren-gewerkt");
+  //   const outputParagraphLeer: any = document.getElementById("output-gewerkt");
 
-    const handleRangeChange = (event: any, nummer: number) => {
-      if (nummer == 0) {
-        outputParagraph.textContent = event.target.value;
-      } else {
-        outputParagraphLeer.textContent = event.target.value;
-      }
-    };
+  //   const handleRangeChange = (event: any, nummer: number) => {
+  //     if (nummer == 0) {
+  //       outputParagraph.textContent = event.target.value;
+  //     } else {
+  //       outputParagraphLeer.textContent = event.target.value;
+  //     }
+  //   };
 
-    rangeInput.addEventListener("input", (event: any) =>
-      handleRangeChange(event, 0)
-    );
-    rangeInputLeer.addEventListener("input", (event: any) =>
-      handleRangeChange(event, 1)
-    );
+  //   rangeInput.addEventListener("input", (event: any) =>
+  //     handleRangeChange(event, 0)
+  //   );
+  //   rangeInputLeer.addEventListener("input", (event: any) =>
+  //     handleRangeChange(event, 1)
+  //   );
 
-    // Cleanup function to remove event listener when changing page by router, since we're using SPA.
-    return () => {
-      rangeInput.removeEventListener("input", handleRangeChange);
-    };
-    // return in useEffect weergeeft niets op de pagina, kan een beetje raar voelen maar dit is gewoon cleanup.
-  }, []);
+  //   // Cleanup function to remove event listener when changing page by router, since we're using SPA.
+  //   return () => {
+  //     rangeInput.removeEventListener("input", handleRangeChange);
+  //   };
+  //   // return in useEffect weergeeft niets op de pagina, kan een beetje raar voelen maar dit is gewoon cleanup.
+  // }, []);
   // We gebruiken een lege array om te zeggen tegen React dat het useEffect maar één keer hoeft te worden gebruikt OOIT, zelfs als useState het volledige component opnieuw probeert te laden.
   // https://dev.to/csituma/why-we-use-empty-array-with-useeffect-iok
   // Als we de array niet neerzetten zal dat bijvoorbeeld problemen kunnen geven als je in je useEffect data fetched van een API en later met useState probeert te updaten want useState zou het volledige component opnieuw laten renderen waardoor de useState fetch onnodig zou worden.
 
+  // const dynamischRef: any = useRef(null);
+
+  const handleUrenGelerdChange = (
+    event: any,
+    output: string,
+    origin: string
+  ) => {
+    const newValue: number = event.target.value;
+    const paragraph = document.getElementById(output);
+    paragraph.textContent = newValue;
+  };
+
   return (
     <>
-      <section className="bg-indigo-300 flex">
-        <div className=" m-auto ">
-          <div>
+      <section className=" flex">
+        <div className=" bg-indigo-100 m-auto w-2/3">
+          <div className="">
             <h1 className="text-center">Hoeveel uren heb je gewerkt?</h1>
-            <form className="grid grid-cols-2">
-              <div className="m-0">
+            <form className="grid grid-cols-2 gap-4 ">
+              <div className="flex place-content-center items-center items-stretch">
                 <label htmlFor="uren-geleerd" className="">
                   Uren geleerd:
                 </label>
@@ -52,28 +66,43 @@ const AddHours = () => {
                   type="range"
                   id="uren-geleerd"
                   name="uren-geleerd"
-                  max="12"
+                  max="8"
                   min="0"
                   defaultValue="0"
-                  className="accent-orange-600"
+                  className="accent-orange-600 ml-2 mr-2"
+                  onChange={(event) => {
+                    handleUrenGelerdChange(
+                      event,
+                      "output-geleerd",
+                      "uren-geleerd"
+                    );
+                  }}
                 />
+                {/* <p id="output-geleerd" className="inline " ref={dynamischRef}> */}
                 <p id="output-geleerd" className="inline">
                   0
                 </p>
               </div>
 
-              <div>
+              <div className="m-0">
                 <label htmlFor="uren-gewerkt" className="">
-                  Uren gewerkt
+                  Uren gewerkt:
                 </label>
                 <input
                   type="range"
                   id="uren-gewerkt"
                   name="uren-gewerkt"
                   min="0"
-                  max="12"
+                  max="8"
                   defaultValue="0"
                   className="accent-purple-600"
+                  onChange={(event) => {
+                    handleUrenGelerdChange(
+                      event,
+                      "output-gewerkt",
+                      "uren-gewerkt"
+                    );
+                  }}
                 />
                 <p id="output-gewerkt" className="inline ">
                   0
