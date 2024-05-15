73e9cfd0b95b35e6eadfe498ff996b83a0b13ac6
hlwalZ <ajustytmy@gmail.com>
2024-05-15 22:22:04 +0200

    BarChart test toegevoegd

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,12 +1,28 @@
-
+import BarChart from "../components/BarChart";
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
-    return(
-        <>
-        <div>Dit is de home page</div>
-        </>
-    )
-}
+  const data2 = () => {
+    const getData = async () => {
+      const res = await fetch("/api/tijden");
+      const data = await res.json();
+      return data;
+    };
+    getData();
+  };
 
+  const data = [
+    { category: "A", value: 10 },
+    { category: "B", value: 20 },
+    { category: "C", value: 30 },
+  ];
 
-export default HomePage
+  return (
+    <>
+      <div>Dit is de home page</div>
+      <BarChart data={data} />
+    </>
+  );
+};
+
+export default HomePage;
